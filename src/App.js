3ff6c0ecfe5eb85807cbb9f56049d614420c1507
Anderson Aparecido Santos Gomes <anderson.gomes@b2wdigital.com>
2022-11-01 22:38:14 -0300

    Criando Funcoes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import React from 'react'
 let initialState = [
   {
     id: 1,
-    prioridade : 'Baixa',
-    titulo : 'Primeira Atividade',
+    prioridade: '1',
+    titulo: 'Primeira Atividade',
     descricao: 'Primeira Atividade'
   },
   {
     id: 2,
-    prioridade : 'Alta',
-    titulo : 'Segunda Atividade',
+    prioridade: '3',
+    titulo: 'Segunda Atividade',
     descricao: 'Segunda Atividade'
   }
 ]
@@ -27,13 +27,48 @@ export const App = () => {
 
     const atividade = {
       id: document.getElementById('id').value,
-      prioridade  : document.getElementById('prioridade').value,
-      titulo : document.getElementById('titulo').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
       descricao: document.getElementById('descricao').value
     }
 
     setAtividades([...atividades, { ...atividade }]);
   }
+
+  function prioridadelabel(param) {
+    switch (param) {
+      case '1':
+        return 'Baixa';
+      case '2':
+        return 'Normal';
+      case '3':
+        return 'Alta';
+      default:
+        return 'Não Definido'
+
+    }
+  }
+
+
+  function prioridadeStyle(param, icone) {
+    switch (param) {
+      case '1':
+        return icone ? 'smile' : 'success';
+      case '2':
+        return icone ? 'meh' : 'dark';
+      case '3':
+        return icone ? 'frown' : 'warning';
+      default:
+        return 'Não Definido'
+    }
+  }
+
+  function deletarAtividade(id) {
+    const atividadeFiltradas = atividades.filter(atividade => atividade.id !== id);
+    setAtividades([...atividadeFiltradas]);
+  }
+
+
 
   return (
     <>
@@ -41,15 +76,22 @@ export const App = () => {
         <form className=" row g-3 ">
           <div className="col-md-6">
             <label htmlFor="id" className="form-label">Id</label>
-            <input className="form-control form-control-sm" type="text" id="id" name="id" placeholder="id" />
+            <input
+              className="form-control form-control-sm"
+              type="text"
+              id="id"
+              name="id"
+              placeholder="id"
+              readOnly
+              value={Math.max.apply(Math, atividades.map(item => item.id)) + 1} />
           </div>
           <div className="col-md-6">
             <label htmlFor="prioridade" className="form-label">Prioridade</label>
             <select name="prioridade" id="prioridade" className="form-select form-select-sm">
-               <option defaultValue="0">Selecione</option>
-               <option value="Baixa">Baixa</option>
-               <option value="Normal">Normal</option>
-               <option value="Alta">Alta</option>
+              <option defaultValue="0">Selecione</option>
+              <option value="1">Baixa</option>
+              <option value="2">Normal</option>
+              <option value="3">Alta</option>
             </select>
           </div>
           <div className="col-md-6">
@@ -69,16 +111,17 @@ export const App = () => {
         </form>
         <div className="mt-3">
           {atividades.map(ativ => (
-            <div className="card mb-3 shadow-sm p-1" key={ativ.id}>
+            <div className={"card mb-3 shadow-sm p-1 border-" + prioridadeStyle(ativ.prioridade)}
+              key={ativ.id}>
               <div className="card-body">
                 <div className="d-flex justify-content-between">
                   <h5 className="card-title">
                     <span className="badge rounded text-bg-secondary me-1">{ativ.id}</span> - {ativ.titulo}
                   </h5>
                   <h6>Prioridade :
-                    <span className="ms-1 text-black">
-                      <i className="ms-2 me-2 far fa-meh"></i>
-                      {ativ.prioridade}
+                    <span className={"ms-1 text-" + prioridadeStyle(ativ.prioridade)}>
+                      <i className={"ms-2 me-2 far fa-" + prioridadeStyle(ativ.prioridade, true)}></i>
+                      {prioridadelabel(ativ.prioridade)}
                     </span>
                   </h6>
                 </div>
@@ -104,8 +147,9 @@ export const App = () => {
                         "--bs-btn-padding-y": ".25rem",
                         "--bs-btn-font-size": ".75rem"
                       }
-                    }>
-                      <i className="fas fa-trash me-2"></i>
+                    }
+                    onClick={() => deletarAtividade(ativ.id)}>
+                    <i className="fas fa-trash me-2"></i>
                     Deletar
                   </button>
                 </div>
